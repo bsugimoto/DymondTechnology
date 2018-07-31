f07e815fa5368ad11656f6f91c9b3ab7d48896b4
Bryan <git.bryan.sugimoto@gmail.com>
2018-07-30 17:00:06 -0700

    removed forgot password

--- myproject/urls.py
+++ myproject/urls.py
@@ -22,13 +22,13 @@ urlpatterns = [
     url(r'^login/$', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     url(r'^logout/$', auth_views.LogoutView.as_view(), name='logout'),
 
-    url(r'^reset/$',
-        auth_views.PasswordResetView.as_view(
-            template_name='password_reset.html',
-            email_template_name='password_reset_email.html',
-            subject_template_name='password_reset_subject.txt'
-        ),
-        name='password_reset'),
+    # url(r'^reset/$',
+    #     auth_views.PasswordResetView.as_view(
+    #         template_name='password_reset.html',
+    #         email_template_name='password_reset_email.html',
+    #         subject_template_name='password_reset_subject.txt'
+    #     ),
+    #     name='password_reset'),
     url(r'^reset/done/$',
         auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'),
         name='password_reset_done'),
